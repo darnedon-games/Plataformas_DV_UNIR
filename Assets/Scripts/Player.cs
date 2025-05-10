1df20614d1b9e50f29ea1ee405559590cef1b4f6
darnedon-test <darnedon@gmail.com>
2025-05-10 14:25:12 +0200

    Farolas, extension mapa y animaciones jugador. Añadidos paquetes unity de farola y enemigos

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,21 +6,62 @@ public class Player : MonoBehaviour
     private float inputH;
     [SerializeField] private float velocidadMovimiento;
     [SerializeField] private float fuerzaSalto;
+    private Animator anim;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputH = Input.GetAxisRaw("Horizontal");
-        rb.linearVelocity = new Vector2(inputH*velocidadMovimiento,rb.linearVelocity.y);
+        Movimiento();
 
+        Saltar();
+
+        Atacar();
+    }
+
+    private void Atacar()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("attack");
+        }
+    }
+
+    private void Saltar()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector2.up*fuerzaSalto, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+            anim.SetTrigger("jump");
+        }
+    }
+
+    private void Movimiento()
+    {
+        inputH = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(inputH * velocidadMovimiento, rb.linearVelocity.y);
+
+        if (inputH != 0) // Si hay movimiento
+        {
+            anim.SetBool("running", true);
+            if (inputH > 0) // Se mueve hacia la derecha
+            {
+                transform.eulerAngles = Vector3.zero;
+            }
+            else // Se mueve hacia la izquierda
+            {
+                transform.eulerAngles = new Vector3(0,180,0);
+            }
+        }
+        else
+        {
+            anim.SetBool("running", false);
         }
     }
 }
