14ad94ad77376bf5d056b3013202eaf34ebc7d72
darnedon-test <darnedon@gmail.com>
2025-05-17 15:02:38 +0200

    añadidos bat y slime con sus capas fisicas y la capa y tag para el player

---
+++ Assets/Scripts/Slime.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+
+public class Slime : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float velocidadPatrulla;
+    private Vector3 destinoActual;
+    private int indiceActual = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        destinoActual = waypoints[indiceActual].position;
+        StartCoroutine(Patrulla());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Patrulla()
+    {
+        while (true)
+        {
+            while (transform.position != destinoActual)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, destinoActual, velocidadPatrulla * Time.deltaTime);
+                yield return null;
+            }
+            DefinirNuevoDestino();
+        }
+    }
+
+    private void DefinirNuevoDestino()
+    {
+        indiceActual++;
+        if (indiceActual >= waypoints.Length)
+        {
+            indiceActual = 0;
+        }
+        destinoActual = waypoints[indiceActual].position;
+        EnfocarDestino();
+    }
+
+    private void EnfocarDestino()
+    {
+        if (destinoActual.x > transform.position.x)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D elOtro)
+    {
+        if (elOtro.gameObject.CompareTag("DeteccionPlayer"))
+        {
+            Debug.Log("Player detectado");
+        }
+        else if (elOtro.gameObject.CompareTag("PlayerHitBox"))
+        {
+            Debug.Log("Player atravesado");
+        }
+    }
+}
