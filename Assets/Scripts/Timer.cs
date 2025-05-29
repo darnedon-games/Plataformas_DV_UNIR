13ca4407b25ffebbcadeb4d440bf1d7f2484bac0
darnedon-test <darnedon@gmail.com>
2025-05-29 14:27:33 +0200

    parada timer cuando se gana o se pierde en el juego

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,23 +4,34 @@ using UnityEngine;
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI tiempoJuego;
+    [SerializeField] private Player player;
+    [SerializeField] private Boss boss;
+    
     private float tiempoTranscurrido;
     private int minutos;
     private int segundos;
+    private bool isRunning;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        isRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        tiempoTranscurrido += Time.deltaTime;
-        minutos = Mathf.FloorToInt(tiempoTranscurrido / 60);
-        segundos = Mathf.FloorToInt(tiempoTranscurrido % 60);
+        if (isRunning)
+        {
+            tiempoTranscurrido += Time.deltaTime;
+            minutos = Mathf.FloorToInt(tiempoTranscurrido / 60);
+            segundos = Mathf.FloorToInt(tiempoTranscurrido % 60);
 
-        tiempoJuego.text = string.Format("Time: {0:00}:{1:00}", minutos, segundos);
+            tiempoJuego.text = string.Format("Time: {0:00}:{1:00}", minutos, segundos);
+        }
+        if ((player == null)||(boss == null))
+        {
+            isRunning = false;
+        }
     }
 }
